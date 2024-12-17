2da6f2de46015f749774e098353c4cf9a9285043
WhatASnowflake <elizabeth.elliott3@nhs.scot>
2024-12-17 15:18:00 +0000

    Update streamlit_app.py
    Adding details for search_on col

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,15 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # Load fruit selection data
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# Convert the snowpark dataframe to a pandas dataframe to use the loc function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -38,6 +46,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         # Display smoothiefroot nutrition information
         st.subheader(fruit_chosen + ' Nutrition Information')
